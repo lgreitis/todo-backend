7fa8837a29ca9d431621f99720bd5c17012550de
lgreitis <lukas.greicius@ktu.edu>
2022-10-11 23:23:14 +0300

    Jest fixes

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
+};
+
+export default config;
