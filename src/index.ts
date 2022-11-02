61fbb2e26776d4001f540c3a25aa8211cad2922d
lgreitis <lukas.greicius@ktu.edu>
2022-11-02 03:06:13 +0200

    undo hocuspocus for now

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-// import expressConfig from '@config/express';
-import hocuspocusConfig from '@config/hocuspocus';
-// import { NODE_ENV, PORT } from '@constants';
-// import {
-//   authRoute,
-//   indexRoute,
-//   userRoute,
-//   directoryRoute,
-//   organizationRoute,
-//   inviteRoute,
-// } from '@routes';
+import expressConfig from '@config/express';
+// import hocuspocusConfig from '@config/hocuspocus';
+import { NODE_ENV, PORT } from '@constants';
+import {
+  authRoute,
+  indexRoute,
+  userRoute,
+  directoryRoute,
+  organizationRoute,
+  inviteRoute,
+} from '@routes';
 // import { serve } from 'swagger-ui-express';
-// import { logger } from '@utils/logger';
-// import * as http from 'node:http';
+import { logger } from '@utils/logger';
+import * as http from 'node:http';
 // import { serve } from 'swagger-ui-express';
 import { prisma } from './config/prisma';
 
 const startServer = async () => {
-  // const app = expressConfig([
-  //   authRoute,
-  //   userRoute,
-  //   indexRoute,
-  //   directoryRoute,
-  //   organizationRoute,
-  //   inviteRoute,
-  // ]);
-  // const httpServer = http.createServer(app);
-  const hocuspocusServer = hocuspocusConfig();
+  const app = expressConfig([
+    authRoute,
+    userRoute,
+    indexRoute,
+    directoryRoute,
+    organizationRoute,
+    inviteRoute,
+  ]);
+  const httpServer = http.createServer(app);
+  // const hocuspocusServer = hocuspocusConfig();
 
   // const server = hocuspocusServer.httpServer;
-  await hocuspocusServer.listen();
+  // await hocuspocusServer.listen();
 
-  // httpServer.listen(PORT, () => {
-  //   logger.info(`=================================`);
-  //   logger.info(`ENV: ${NODE_ENV}`);
-  //   logger.info(`PORT: ${PORT}`);
-  //   logger.info(`=================================`);
-  // });
+  httpServer.listen(PORT, () => {
+    logger.info(`=================================`);
+    logger.info(`ENV: ${NODE_ENV}`);
+    logger.info(`PORT: ${PORT}`);
+    logger.info(`=================================`);
+  });
 };
 
 startServer()
